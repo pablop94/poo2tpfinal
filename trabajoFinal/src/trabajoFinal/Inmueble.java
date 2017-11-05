b27116f981e028c7b93e8117bccd295c19fe23e6
Dova81 <33400774+Dova81@users.noreply.github.com>
2017-11-05 16:19:50 -0300

    Putos todos
    putos todos

---
+++ trabajoFinal/src/trabajoFinal/Inmueble.java
@@ -0,0 +1,13 @@
+package trabajoFinal;
+
+public class Inmueble {
+	private String propietario;
+	private String direccion;
+	private String servicios;
+	private String tipoDeInmuble;
+	private String pais;
+	private String ciudad;
+	private Double superficie;
+	private String fotos;
+	private Double capacidad;
+}
