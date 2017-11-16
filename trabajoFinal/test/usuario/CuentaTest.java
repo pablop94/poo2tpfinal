f0236ea98ad924c4accb246c7363dc3e04beac6e
Pablo <pablopissi@gmail.com>
2017-11-15 22:55:13 -0300

    test de cuenta

--- trabajoFinal/test/usuario/CuentaTest.java
+++ trabajoFinal/test/usuario/CuentaTest.java
@@ -1,19 +1,25 @@
 package usuario;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class CuentaTest {
 
+	private Cuenta cuenta;
+
 	@Before
 	public void setUp() throws Exception {
+		cuenta = new Usuario("pepe", "mail", "42277777", mock(Rol.class));
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_constructor() {
+		assertEquals(cuenta.obtenerNombre(), "pepe");
+		assertEquals(cuenta.obtenerMail(), "mail");
+		assertEquals(cuenta.obtenerTelefono(), "42277777");
 	}
 
 }
